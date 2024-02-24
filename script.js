37237fa7954b0920a72c5005596774ec9b3ff14b
Dmytro <dimitro83@gmail.com>
2024-02-24 20:49:34 +0200

    hw 3 4 5 ready

--- script.js
+++ script.js
@@ -1,120 +1,29 @@
-let answerOne = prompt('Enter any word', );
-let answerTwo = prompt('Enter another word', );
-let answerThree = prompt('Enter one more word', );
-let result1 = answerOne + ' ' + answerTwo + ' ' + answerThree;
-console.log('Result homework 2.1:', result1);
+//homework 3
+let numberOne = +prompt('Enter first number:');
+let numberTwo = +prompt('Enter second number:');
 
+let sumNumbers = numberOne + numberTwo;
+let minusNumbers = numberOne - numberTwo;
+let multNumbers = numberOne * numberTwo;
+let divNumbers = numberOne / numberTwo;
 
-let number = prompt('Enter five digit number:');
-let one = number[0];
-let two = number[1];
-let three = number[2];
-let four = number[3];
-let five = number[4];
-let result2 = `${one} ${two} ${three} ${four} ${five}`;
-console.log('Result homework 2.2:', result2);
+alert(`User entered ${numberOne} and ${numberTwo}:
+  ${numberOne} + ${numberTwo} = ${sumNumbers}
+  ${numberOne} - ${numberTwo} = ${minusNumbers}
+  ${numberOne} * ${numberTwo} = ${multNumbers}
+  ${numberOne} / ${numberTwo} = ${divNumbers}`
+  );
 
-// let result2 = number.split('');
-// let answer = result2.join(' ');
-// console.log('Result homework 2.2:', answer);
+//homework 4
+let userHours = +prompt('Enter how many hours: ');
+const secondsInHour = 3600;
+let userSeconds = userHours * secondsInHour;
+alert(`The ${userHours} hours is ${userSeconds} seconds`);
 
+//homework 5
+let numOne = +prompt(`Enter first number: `);
+let numTwo = +prompt(`Enter second number`);
+let numThree = +prompt(`Enter third number`);
 
-
-
-
-// ******lesson_03****** 
-
-// console.log(x);
-// var x = 5;
-
-// // итепритатор читає так //
-// var x;
-// console.log(x);
-// x = 5;
-
-
-
-// let test = 1;
-// const num = 2;
-
-// // итепритатор читає так //
-// {
-//     let test = 1;
-//     const num = 2;
-// }
-
-// const typeVar = typeof test;
-
-// 0,1 + 0,2
-// 2
-// 0,1+0,2
-// 2
-// 0.1 + 0.2
-// 0.30000000000000004
-//  "sdfdsf".lenth
-// undefined
-// "ssdgfdgsg".length
-// 9
-// let str = "asdffdf";
-// undefined
-// str.length
-// 7
-// str[str.length - 1]
-// 'f'
-
-// 5
-// 3 /1
-// 3
-// 3 % 1
-// 0
-// 3 % 2
-// 1
-
-// parseInt('100.99px');
-// parseFloat('10.333px');
-
-// parseFloat('10.333px');
-
-// let sum = 0.1 + 0.2;
-// undefined
-// let res = sum.toFixed(2);
-// undefined
-// typeof res
-// 'string'
-// +res
-// 0.3
-// console.log(+res);
-// VM392:1 0.3
-// undefined
-// let sum = 25 + 25;
-// undefined
-//     asdfd
-// VM473:1 Uncaught ReferenceError: asdfd is not defined
-//     at <anonymous>:1:5
-// (anonymous) @ VM473:1
-//     SVGFEDiffuseLightingElement
-// ƒ SVGFEDiffuseLightingElement() { [native code] }
-//     let sum = 0.1 + 0.3;
-// undefined
-// let res = +sum.toFixed(2);
-// undefined
-// typeof res
-// 'number'
-// let num = 11, str = "test';
-// VM759:1 Uncaught SyntaxError: Invalid or unexpected token
-// let num = 12, str = 'sdfs';
-// VM831:1 Uncaught SyntaxError: Identifier 'num' has already been declared
-// let dwa = 12, str = 'fgh';
-// undefined
-// isNaN(11)
-// false
-// isNaN('11')
-// false
-// parseFloat('12.202px')
-// 12.202
-// parseFloat('12.202px')
-// 12.202
-// parseInt('asda')
-// NaN
-// parseInt('10.255px')
-// 10
+let average = (numOne + numTwo + numThree) / 3;
+alert(`The average of your numbers is ${average}`);
