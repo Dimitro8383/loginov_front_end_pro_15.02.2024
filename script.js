31cffd05e028b4d82f8bf75783062bf5daf704e9
Dmytro <dimitro83@gmail.com>
2024-03-12 19:53:18 +0200

    hw12 ready

--- script.js
+++ script.js
@@ -1,34 +1,14 @@
-//hw11
+// hw12
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-let arr_1 = [];
-
-for (let i = 1; i <= 5; i++) {
-  arr_1.push(i);
+function generateKey(length, content) {
+  let resultKey = [];
+  let contentLength = content.length;
+  for (let i = 1; i <= length; i++) {
+    resultKey.push(characters[Math.floor(Math.random() * characters.length)]);
+  }
+  return (resultKey);
 }
 
-console.log(arr_1);
-
-function removeElement(array, item) {
-  let removeIndex = array.indexOf(item);
-    if (removeIndex != -1) {
-      array.splice(removeIndex, 1);
-    }
-}
-removeElement(arr_1, 500);
-console.log(arr_1);
-
-removeElement(arr_1, 1);
-console.log(arr_1);
-
-removeElement(arr_1, 5);
-console.log(arr_1);
-
-removeElement(arr_1, 3);
-console.log(arr_1);
-
-removeElement(arr_1, 2);
-console.log(arr_1);
-
-removeElement(arr_1, 4);
-console.log(arr_1);
+console.log(generateKey(16, characters));
 
